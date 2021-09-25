f53869441a8abac9219813b34d9233278a8916e8
zouyalong <zouyalong@bytedance.com>
2021-09-25 22:50:31 +0800

    debug: more log
    Change-Id: I0c5be0565dfb3d1c3743f6701b339d1a2f0cd0d5

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,7 +1,19 @@
+//! # 全局属性
+//! - `#![no_std]`  
+//!   禁用标准库
 #![no_std] // 告知编译器不使用 std 库，而使用 core
+//! - `#![no_main]`  
+//!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口
 #![no_main] // 没有一般意义上的main
+//! # 一些 unstable 的功能需要在 crate 层级声明后才可以使用
+//! - `#![feature(llvm_asm)]`  
+//!   内嵌汇编
 #![feature(llvm_asm)]
+//! - `#![feature(global_asm)]`  
+//!   内嵌整个汇编文件
 #![feature(global_asm)] // 此 feature 用于嵌入全局汇编
+//! - `#![feature(panic_info_message)]`  
+//!   panic! 时，获取其中的信息并打印
 #![feature(panic_info_message)] // 通过 PanicInfo::message 获取报错信息
 
 
--- os/src/std/io/mod.rs
+++ os/src/std/io/mod.rs
@@ -5,14 +5,17 @@ use crate::sbi::console_putchar;
 /// 标准输出流
 const stdout: usize = 1;
 
-struct Stdout;
+pub(crate) struct Stdout;
 
 impl Write for Stdout {
     /// 调用 sys_write 系统调用，向 stdout 写数据
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let mut buffer = [0u8; 4];
         // 使用 sbi 方法，不能用 syscall
         for c in s.chars() {
-            console_putchar(c as usize);
+            for code_ptr in c.encode_utf8(&mut buffer).as_bytes().iter() {
+                console_putchar(*code_ptr as usize);
+            }
         }
         Ok(())
     }
